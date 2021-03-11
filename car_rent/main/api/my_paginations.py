2d55cc537aea1c88cce5cf67749201a905562ecf
Agil89 <agil.makhmudov@mail.ru>
2021-03-11 12:29:26 +0400

    pagination with page

--- car_rent/main/api/my_paginations.py
+++ car_rent/main/api/my_paginations.py
@@ -1,7 +1,8 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 
-class PagePagination(LimitOffsetPagination):
-    default_limit = 1
-    limit_query_param = 'limit'
-    offset_query_param = 'offset'
-    max_limit = 20
+class PagePagination(PageNumberPagination):
+    # default_limit = 1
+    # limit_query_param = 'limit'
+    # offset_query_param = 'offset'
+    # max_limit = 20
+    page_size = 3
